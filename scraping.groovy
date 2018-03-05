a7a1c065f82f4510fd65f29ba3a2cb5ec9b601b3
sprine168 <sprine@missouriwestern.edu>
2018-03-04 22:08:08 -0600

    Everything Working Except...
    Everything is working except for the menu and sub lists.

--- scraping.groovy
+++ scraping.groovy
@@ -4,95 +4,25 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.select.Elements
 
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException;
-
 /*
 * Steven Prine
 * csc-346
 * Prof. Noynaert
 */
 
-class Subject {
-
-    def subject, abbrv = ""
-
-    Subject(abbrv, subject) {
-        this.subject = subject
-        this.abbrv = abbrv
-    }
-
-    @Override
-    String toString() {
-        return "$abbrv $subject"
-    }
-}
-
-class Department {
-
-    def abbrv, department
-
-    public Department(abbrv, department) {
-        this.abbrv = abbrv
-        this.department = department
-    }
-
-    @Override
-    String toString() {
-        return "$abbrv $department"
-    }
-}
-
-//Scraping Subjects
-def subjectSearch() throws IO {
+def subjectCreate() throws IO {
     def website = "https://aps2.missouriwestern.edu/schedule/Default.asp?tck=201910"
     Document document = Jsoup.connect(website).get()
 
-    Elements rows = document.select("#subject > option[value]")
-    rows.remove(0)
-    def abbrv
-    def subject
-    Subject subjects
-
-    def data = []
-
-    rows.each { row ->
-        def subjectCells = row.select("option[value]")
-        subject = subjectCells.text()
-
-        def abbrvCells = row.attr("value")
-        abbrv = abbrvCells
-
-        subjects = new Subject(abbrv, subject)
-        data.add(subjects)
-    }
-
-    //Will eventually be entering data here programatically to subject table
-    data.each {
-        println it
-    }
-}
-
-//Scraping Departments
-def departmentSearch() throws IO {
-    def website = "https://aps2.missouriwestern.edu/schedule/Default.asp?tck=201910"
-    Document documentDep = Jsoup.connect(website).get()
-
     def sql = Sql.newInstance("jdbc:sqlite:jsoup.db", "org.sqlite.JDBC")
 
-    Elements rows = documentDep.select("#department option")
-    Department departments
-
-    def dataDep = []
-
+    Elements rows = document.select("#subject > option")
     rows.each { row ->
-        println row
         def abbrvCells = row.attr("value")
         def fullName = row.text()
 
         if (!abbrvCells.equals("ALL")) {
-            def insert = "Insert into Departments(ABBRV, Department) VALUES(? , ?)"
+            def insert = "Insert into Subjects(abbrv, Subject) VALUES(? , ?)"
             def params = [abbrvCells, fullName]
             sql.executeInsert(insert, params)
         }
@@ -100,7 +30,65 @@ def departmentSearch() throws IO {
     sql.close()
 }
 
-subjectSearch()
-departmentSearch()
+def departmentCreate() throws IO {
+    def website = "https://aps2.missouriwestern.edu/schedule/Default.asp?tck=201910"
+    Document documentDep = Jsoup.connect(website).get()
 
+    def sql = Sql.newInstance("jdbc:sqlite:jsoup.db", "org.sqlite.JDBC")
 
+    Elements rows = documentDep.select("#department option")
+    rows.each { row ->
+        def abbrvCells = row.attr("value")
+        def fullName = row.text()
+
+        if (!abbrvCells.equals("ALL")) {
+            def insert = "Insert into Subjects(ABBRV, Subject) VALUES(? , ?)"
+            def params = [abbrvCells, fullName]
+            sql.executeInsert(insert, params)
+        }
+    }
+    sql.close()
+}
+
+def departmentDelete() throws IO {
+    def sql = Sql.newInstance("jdbc:sqlite:jsoup.db", "org.sqlite.JDBC")
+    def delete = "DELETE FROM Departments Where EXISTS(Select * from Departments)"
+    sql.execute(delete)
+    sql.close()
+}
+
+def subjectsDelete() throws IO {
+    def sql = Sql.newInstance("jdbc:sqlite:jsoup.db", "org.sqlite.JDBC")
+    def delete = "DELETE FROM Subjects Where EXISTS(Select * from Subjects)"
+    sql.execute(delete)
+    sql.close()
+}
+
+def printSubjects() throws IO {
+    def sql = Sql.newInstance("jdbc:sqlite:jsoup.db", "org.sqlite.JDBC")
+    sql.eachRow('SELECT * FROM Subjects') {
+        println it
+    }
+    sql.close()
+}
+
+def printDepartments() throws IO {
+    def sql = Sql.newInstance("jdbc:sqlite:jsoup.db", "org.sqlite.JDBC")
+
+    sql.eachRow('SELECT * FROM Departments'){
+        print it
+    }
+    sql.close()
+}
+
+//a in menu is to create departments table
+//b in menu is to create subjects table
+println "A: Erase and build Subjects Table\tB:Erase and Build Departments Table\n " +
+        "C: Print Departments Tablet\tPrint the report of disciplines by Department\n" +
+        "D: Print Departments Table\tPrint the report of disciplines by Department\n" +
+        "Erase and build sections data"
+
+def menu
+
+printSubjects()
+printDepartments()
